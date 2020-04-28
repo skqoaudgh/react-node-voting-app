f92b8adefc1281680596e0ce697356b70c26671e
myungho <skqoaudgh2@hanmail.net>
2020-04-28 15:59:30 +0900

    JWT 추가

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,10 +1,15 @@
+const jwt = require('jsonwebtoken');
+
 const db = require('../models');
 
 exports.register = async (req, res, next) => {
   try {
     const user = await db.User.create(req.body);
     const { id, username } = user;
-    res.status(201).json({ id, username });
+
+    const token = jwt.sign({ id, username }, process.env.SECRET);
+
+    res.status(201).json({ id, username, token });
   } catch (err) {
     if (err.code === 11000) {
       err.message = 'the username is already existed';
@@ -20,7 +25,9 @@ exports.login = async (req, res, next) => {
     const valid = await user.comparePassword(req.body.password);
 
     if (valid) {
-      res.json({ id, username });
+      const token = jwt.sign({ id, username }, process.env.SECRET);
+
+      res.json({ id, username, token });
     } else {
       throw new Error();
     }
