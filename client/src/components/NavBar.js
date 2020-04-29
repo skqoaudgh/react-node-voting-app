d28f5c292e0b11eb5bd70168d7d7f2ff17fe5e08
myungho <skqoaudgh2@hanmail.net>
2020-04-29 18:24:17 +0900

    네비게이션 바 추가

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { logout } from '../store/actions/auth';
+
+const NavBar = ({ auth, logout }) => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/register">회원가입</Link>
+      </li>
+      <li>
+        <Link to="/login">로그인</Link>
+      </li>
+      <li>
+        <a onClick={logout}>Logout</a>
+      </li>
+    </ul>
+    {auth.isAuthenticated && <p>Logged in as a {auth.user.username}</p>}
+  </div>
+);
+
+export default connect((store) => ({ auth: store.auth }), { logout })(NavBar);
