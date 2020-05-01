d3216b84fa89ae863701032dfc02b56b3fcbceee
myungho <skqoaudgh2@hanmail.net>
2020-05-01 16:24:45 +0900

    Poll에 대한 리듀서, 액션 추가

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -2,8 +2,11 @@ import { combineReducers } from 'redux';
 
 import error from './error';
 import auth from './auth';
+import { polls, currentPoll } from './poll';
 
 export default combineReducers({
   error,
   auth,
+  polls,
+  currentPoll,
 });
